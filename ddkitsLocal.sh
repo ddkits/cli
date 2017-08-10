c4da1c9cebc93f87b8dfc4aa0e420d4f425d12fc
Sam Elayyoub <melayyoub@outlook.com>
2017-08-10 15:24:47 -0700

    Elgg 2.x new with DDkits

--- ddkitsLocal.sh
+++ ddkitsLocal.sh
@@ -350,7 +350,7 @@ DDKITS_PLATFORM='Please pick which platform you want to install: '
 # 
 # Setup options Please make sure of all options before publish pick list 
 # 
-options=("Drupal" "Wordpress" "Joomla" "Laravel" "LAMP/PHP5" "LAMP/PHP7" "Umbraco" "Magento" "DreamFactory" "Contao" "Silverstripe" "Cloud" "Symfony" "Expression Engine" "Quit")
+options=("Drupal" "Wordpress" "Joomla" "Laravel" "LAMP/PHP5" "LAMP/PHP7" "Umbraco" "Magento" "DreamFactory" "Contao" "Silverstripe" "Cloud" "Symfony" "Expression Engine" "Elgg" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -1966,6 +1966,136 @@ alias ddkc-$DDKITSSITES='docker exec -it '$DDKITSHOSTNAME'_ddkits_contao_web /bi
 #  fixed the alias for machine
 echo "alias ddkc-"$DDKITSSITES"='ddk go && docker exec -it "$DDKITSHOSTNAME"_ddkits_contao_web /bin/bash'" >> ~/.ddkits_alias_web
 echo $SUDOPASS | sudo -S chmod -R 777 ./contao-deploy
+
+            break
+            ;;
+"Elgg")
+
+if [[ ! -d "ddkits-files/elgg/sites" ]]; then
+  mkdir ddkits-files/elgg/sites
+  chmod -R 777 ddkits-files/elgg/sites
+fi
+
+
+    # delete the old environment yml file
+        if [[ -f "ddkits.env.yml" ]]; then
+          rm ddkits.env.yml
+        fi
+        # delete the old environment yml file
+        if [[ -f "ddkitsnew.yml" ]]; then
+          rm ddkitsnew.yml
+        fi
+        # delete the old environment yml file
+        if [[ -f "ddkits-files/elgg/Dockerfile" ]]; then
+          rm ddkits-files/elgg/Dockerfile
+        fi
+        # delete the old environment yml file
+        if [[ -f "ddkits-files/ddkits.fix.sh" ]]; then
+          rm ddkits-files/ddkits.fix.sh
+        fi
+        if [[ -f "ddkits-files/elgg/sites/$DDKITSHOSTNAME.conf" ]]; then
+          rm ddkits-files/elgg/sites/$DDKITSHOSTNAME.conf
+        fi
+
+#  Elgg PHP 5
+
+echo -e '
+<VirtualHost *:80>
+     ServerAdmin melayyoub@outlook.com
+     ServerName '$DDKITSSITES'
+     '$DDKITSSERVERS'
+     DocumentRoot /var/www/html/public
+      ErrorLog /var/www/html/error.log
+     CustomLog /var/www/html/access.log combined
+    <Location "/">
+      Require all granted
+      AllowOverride All
+      Order allow,deny
+      allow from all
+  </Location>
+  <Directory "/var/www/html">
+      Require all granted
+      AllowOverride All
+      Order allow,deny
+      allow from all
+  </Directory>
+</VirtualHost> ' > ./ddkits-files/elgg/sites/$DDKITSHOSTNAME.conf
+
+echo -e '
+FROM ddkits/lamp:latest
+
+MAINTAINER Mutasem Elayyoub "melayyoub@outlook.com"
+
+RUN export TERM=xterm
+
+RUN rm /etc/apache2/sites-enabled/000-default.conf
+COPY sites/'$DDKITSHOSTNAME'.conf /etc/apache2/sites-enabled/'$DDKITSHOSTNAME'.conf
+COPY php.ini /usr/local/etc/php/conf.d/php.ini
+
+# Set the default command to execute
+
+RUN chmod 600 /etc/mysql/my.cnf \
+    && a2enmod rewrite 
+
+RUN apt-get update \
+  && apt-get install build-essential apt-transport-https  -y --force-yes\
+  && echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list \
+  && apt-get update \
+  && apt-get install -y --force-yes nano \
+                   wget \
+                   dialog \
+                   net-tools \
+                   lxc-docker \
+                   ufw \
+                   sudo \
+                   gufw \
+  && apt-get install -y --force-yes apt-transport-https  
+RUN chmod -R 777 /var/www/html 
+
+# Fixing permissions 
+RUN chown -R www-data:www-data /var/www/html
+RUN usermod -u 1000 www-data
+  ' >> ./ddkits-files/elgg/Dockerfile
+
+echo -e 'version: "2"
+
+services:
+  web:
+    build: ./ddkits-files/elgg
+    image: ddkits/elgg:latest
+    depends_on:
+      # Link the Solr container:
+      - "solr"
+      # Link the mariaDB container:
+      - "mariadb"
+    volumes:
+      - ./elgg-deploy:/var/www/html
+    stdin_open: true
+    tty: true
+    container_name: '$DDKITSHOSTNAME'_ddkits_elgg_web
+    networks:
+      - ddkits
+    ports:
+      - "'$DDKITSWEBPORT':80" ' >> ddkits.env.yml
+
+if [[ ! -d "elgg-deploy/public" ]]; then
+  DDKITSFL=$(pwd)
+  echo $DDKITSFL
+  cp ./composer.phar ./elgg-deploy/public/ddkits.phar
+  git clone https://github.com/ddkits/elgg elgg-deploy
+  echo $SUDOPASS | sudo -S chmod -R 777 public ./elgg-deploy
+  cd ./elgg-deploy
+  cd public && php ddkits.phar config --global discard-changes true && php ddkits.phar install -n
+  cd $DDKITSFL
+  echo $SUDOPASS | sudo -S chmod -R 777 ./elgg-deploy/public
+fi
+
+# create get into ddkits container
+echo $SUDOPASS | sudo -S cat ~/.ddkits_alias > /dev/null
+alias ddkc-$DDKITSSITES='docker exec -it '$DDKITSHOSTNAME'_ddkits_elgg_web /bin/bash'
+#  fixed the alias for machine
+echo "alias ddkc-"$DDKITSSITES"='ddk go && docker exec -it "$DDKITSHOSTNAME"_ddkits_elgg_web /bin/bash'" >> ~/.ddkits_alias_web
+echo $SUDOPASS | sudo -S chmod -R 777 ./elgg-deploy
 
             break
             ;;
