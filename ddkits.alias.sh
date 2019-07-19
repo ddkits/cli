f135c880828913bf77d528dbeeb7e86fe4b33351
Mutasem Elayyoub <m22386@live.com>
2019-07-19 09:58:45 -0700

    Fixing Update to start with the config first

--- ddkits.alias.sh
+++ ddkits.alias.sh
@@ -579,7 +579,7 @@ ddk() {
     SOLR     http://solr.YOUR_DOMAIN.ddkits.site
     PhpMyAdmin     http://admin.YOUR_DOMAIN.ddkits.site
 
-    DDKits v3.24
+    DDKits v3.25
         '
   else
     echo 'DDkits build by Mutasem Elayyoub and ready to usesource  www.DDKits.com
