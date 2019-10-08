3745f646ad7d9c536333dc4f663bca5e9d715465
Mutasem Elayyoub <m22386@live.com>
2019-10-08 12:45:33 -0700

    v4.21

--- ddkits.alias.sh
+++ ddkits.alias.sh
@@ -505,7 +505,7 @@ ddk() {
     SOLR     http://solr.YOUR_DOMAIN.ddkits.site
     PhpMyAdmin     http://admin.YOUR_DOMAIN.ddkits.site
 
-    DDKits v4.20
+    DDKits v4.21
         "
   else
     echo "DDkits build by Mutasem Elayyoub and ready to usesource  www.DDKits.com
