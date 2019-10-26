9d5ed127eaf65382d65d3071e6cea6b15de65ac0
Sam Elayyoub <m22386@live.com>
2019-10-25 23:25:49 -0700

    New feature

--- ddkits-files/ddkits/ddkits.call.sh
+++ ddkits-files/ddkits/ddkits.call.sh
@@ -1,9 +1,12 @@
+#!/bin/sh
+
+#  call.sh
+#
+# This system Powered by Mutasem Elayyoub DDKits.com 
+MYIP='dig @resolver1.opendns.com ANY myip.opendns.com +short'
 curl -v --request POST \
-  --url 'https://getfreeapi.com/api/ddk/1_ddkits?domain='${DDKITSSITES}'&platform='${PLATFORM}'&email='${MAIL_ADDRESS}'' \
+  --url 'https://getfreeapi.com/api/ddk/1_ddkits?domain='${DDKITSSITES}'&platform='${PLATFORM}'&opt='${opt}'&email='${MAIL_ADDRESS}'' \
   --header 'Accept: application/json' \
   --header 'Authorization: Bearer '${TO}'' \
-  --header 'Cache-Control: no-cache' \
   --header 'Host: getfreeapi.com' \
-  --header 'User-Agent: PostmanRuntime/7.13.0' \
-  --header 'accept-encoding: gzip, deflate' \
   --header 'cache-control: no-cache'
