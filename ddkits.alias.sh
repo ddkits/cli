0eea8100d61e9f370e05a807357f707437a0efd2
Sam Elayyoub <m22386@live.com>
2020-03-21 13:08:28 -0700

    Cleanup Bash profile with DDKits wrong commands

--- ddkits.alias.sh
+++ ddkits.alias.sh
@@ -542,7 +542,7 @@ ddk() {
     SOLR     http://solr.YOUR_DOMAIN.ddkits.site
     PhpMyAdmin     http://admin.YOUR_DOMAIN.ddkits.site
 
-    DDKits v4.301
+    DDKits v4.302
         "
   else
     echo "DDkits build by Mutasem Elayyoub and ready to usesource  www.DDKits.com
