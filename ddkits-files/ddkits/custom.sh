73923f06587da230affec70f24e479a3c678d993
Sam Elayyoub <m22386@live.com>
2020-08-07 12:52:59 -0700

    Update custom.sh

--- ddkits-files/ddkits/custom.sh
+++ ddkits-files/ddkits/custom.sh
@@ -15,19 +15,19 @@ if [[ -f "${DDKITSFL}/ddkitsnew.yml" ]]; then
   rm $DDKITSFL/ddkitsnew.yml
 fi
 # delete the old environment yml file
-if [[ -f "${DDKITSFL}/ddkits-files/lamp7/Dockerfile" ]]; then
-  rm $DDKITSFL/ddkits-files/lamp7/Dockerfile
+if [[ -f "${DDKITSFL}/ddkits-files/custom/Dockerfile" ]]; then
+  rm $DDKITSFL/ddkits-files/custom/Dockerfile
 fi
 # delete the old environment yml file
 if [[ -f "${DDKITSFL}/ddkits-files/ddkits.fix.sh" ]]; then
   rm $DDKITSFL/ddkits-files/ddkits.fix.sh
 fi
-if [[ -f "${DDKITSFL}/ddkits-files/lamp7/sites/$DDKITSHOSTNAME.conf" ]]; then
-  rm $DDKITSFL/ddkits-files/lamp7/sites/$DDKITSHOSTNAME.conf
+if [[ -f "${DDKITSFL}/ddkits-files/custom/sites/$DDKITSHOSTNAME.conf" ]]; then
+  rm $DDKITSFL/ddkits-files/custom/sites/$DDKITSHOSTNAME.conf
 fi
-if [[ ! -d "${DDKITSFL}/ddkits-files/lamp7/sites" ]]; then
-  mkdir $DDKITSFL/ddkits-files/lamp7/sites
-  chmod -R 777 $DDKITSFL/ddkits-files/lamp7/sites
+if [[ ! -d "${DDKITSFL}/ddkits-files/custom/sites" ]]; then
+  mkdir $DDKITSFL/ddkits-files/custom/sites
+  chmod -R 777 $DDKITSFL/ddkits-files/custom/sites
 fi
 #  LAMP PHP 7
 if [[ ! -d "${DDKITSFL}/ddkits-files/ddkits/ssl" ]]; then
@@ -98,11 +98,11 @@ echo -e '
       allow from all
   </Directory>
 </VirtualHost>
-' >$DDKITSFL/ddkits-files/lamp7/sites/$DDKITSHOSTNAME.conf
+' >$DDKITSFL/ddkits-files/custom/sites/$DDKITSHOSTNAME.conf
 
 echo -e '
 
-FROM ddkits/lamp:7
+FROM ddkits/lamp:7.3
 
 MAINTAINER Mutasem Elayyoub "melayyoub@outlook.com"
 
@@ -113,23 +113,22 @@ RUN ln -sf $DDKITSFL/logs /var/log/nginx/access.log \
 RUN chmod -R 777 /var/www/html
 
 COPY php.ini /etc/php/7.0/fpm/php.ini
+RUN rm -rf /etc/apache2/sites-enabled/no_site.conf
 COPY ./sites/'$DDKITSHOSTNAME'.conf /etc/apache2/sites-enabled/'$DDKITSHOSTNAME'.conf
 
 # Fixing permissions
 RUN chown -R www-data:www-data /var/www/html
 RUN usermod -u 1000 www-data
 
-' >>$DDKITSFL/ddkits-files/lamp7/Dockerfile
+' >$DDKITSFL/ddkits-files/custom/Dockerfile
 
 echo -e 'version: "3.1"
 
 services:
   web:
-    build: $DDKITSFL/ddkits-files/lamp7
-    image: ddkits/lamp7:latest
-
+    image: ddkits/lamp:7.3
     volumes:
-      - $DDKITSFL/lamp7-deploy:/var/www/html
+      - $DDKITSFL/custom-deploy:/var/www/html
     stdin_open: true
     tty: true
     container_name: '$DDKITSHOSTNAME'_ddkits_web
@@ -145,20 +144,19 @@ services:
 echo -e 'Enter your Repo link to start pulling your files into your custom folder  Example
 https://github.com/ddkits/cli.git
 
-File will be cloned into your deploy folder which deploy/'$WEBROOT' folder must be the root of your web
+File will be cloned into your deploy folder which custom-deploy/'$WEBROOT' folder must be the root of your web
 '
 read GITREPO
 
 # Git clone the
-git clone $GITREPO gitfolder
-cp -a gitfolder/. deploy/
-chmod -R 0777 deploy
+git clone $GITREPO gitfoldertmp
+cp -a gitfoldertmp/. custom-deploy/
 
 # create get into ddkits container
 echo $SUDOPASS | sudo -S cat ~/.ddkits_alias >/dev/null
 alias ddkc-$DDKITSSITES='docker exec -it ${DDKITSHOSTNAME}_ddkits_web /bin/bash'
 #  fixed the alias for machine
 echo "alias ddkc-"$DDKITSSITES"='ddk go && docker exec -it "$DDKITSHOSTNAME"_ddkits_web /bin/bash'" >>~/.ddkits_alias_web
-echo $SUDOPASS | sudo -S chmod -R 777 $DDKITSFL/lamp7-deploy
+echo $SUDOPASS | sudo -S chmod -R 777 $DDKITSFL/custom-deploy
 
 cd $DDKITSFL
