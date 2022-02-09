7bcc1ea8d797197cda1e66703f3092f8118ada3b
Sam Elayyoub <m22386@live.com>
2022-02-09 12:32:02 -0800

    Update ddkits.alias.sh

--- ddkits.alias.sh
+++ ddkits.alias.sh
@@ -167,7 +167,7 @@ ddk() {
     echo -e '\nDDKits installed successfully, \nThank you for using DDKits'
  elif [[ $1 = "ip" ]]; then
     # export DDKITSIP=$(docker-machine ip ddkits)
-    if [[ $DDKMACHINE = "1" && $DDKITSVER = "1" ]]; then
+    if [[ $DDKMACHINE = "1" ]]; then
       ddk go
       Docker-machine ls | grep ddkits >/dev/null && export DDKMACHINE=1 || echo 'DDKits container is not using DDKits Docker Machine'
       ddk c | grep ddkits >/dev/null && export DDKITSIP=$(docker-machine ip ddkits) || export DDKITSIP='Please make sure your DDKits container is installed and running'
@@ -599,7 +599,7 @@ ddk() {
     Redis         ddkc-SiteName-cache
 
             **************************
-    DDKits v4.335
+    DDKits v4.336
         "
   else
     echo "DDkits build by Mutasem Elayyoub and ready to usesource  www.DDKits.com
